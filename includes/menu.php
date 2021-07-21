4001e24da7dea968f77f6d17d1eb25e774da49fe
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-07-21 16:01:38 +0800

    adding img for customer

---
+++ includes/menu.php
@@ -0,0 +1,300 @@
+<div class="menu-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center" >
+						<h2 data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="1500">Special Menu</h2>
+						<p data-aos="fade-up"
+     data-aos-anchor-placement="center-bottom">Try our new Latest Product</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row inner-menu-box">
+				<div class="col-3">
+					<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+						<a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">All</a>
+						<a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Drinks</a>
+						<a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Takeout Meal</</a>
+						<a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Dinner</a>
+					</div>
+				</div>
+				
+				<div class="col-9">
+					<div class="tab-content" id="v-pills-tabContent">
+						<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+							<div class="row">
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-01.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Drinks 1</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $7.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-02.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Drinks 2</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $9.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-03.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Drinks 3</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $10.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid lunch">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-04.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Lunch 1</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $15.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid lunch">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-05.jpg"data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Lunch 2</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $18.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid lunch">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-06.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Lunch 3</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $20.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid dinner">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-07.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Dinner 1</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $25.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid dinner">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-08.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Dinner 2</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $22.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid dinner">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-09.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Dinner 3</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $24.79</h5>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+						</div>
+						<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+							<div class="row">
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/drinks/wilkins.png" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Wilkins Water</h4>
+											<h5> 25.00</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/drinks/coke.png" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Coca Cola in cans</h4>
+											<h5> 55.00</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/drinks/1.5.png" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Soft Drinks (1.5 Liters) </h4>
+											<h5> 55.00</h5>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/drinks/melona.png" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Melona </h4>
+											<h5> 55.00</h5>
+										</div>
+									</div>
+								</div>
+
+								
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/drinks/soju.png" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Soju </h4>
+											<h5> 120.00</h5>
+										</div>
+									</div>
+								</div>
+
+																
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/drinks/dutch.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Dutch Milk Delight </h4>
+											<h5> 30.00</h5>
+										</div>
+									</div>
+								</div>
+
+							</div>
+							
+						</div>
+						<div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+							<div class="row">
+								<div class="col-lg-4 col-md-6 special-grid lunch">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/takeout/takeout1.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Rice Meal</h4>
+											<h5> Duo wings with rice (75.00)</h5>
+											<h5> Trio wings with rice (109.00)</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid lunch">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/takeout/fries.png" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Combo Meal</h4>
+											<h5> Chicks and Fries (179.00)</h5>
+											<h5> Box of Fries (129.00)</h5> 
+											<p>Cheese Fondue Overload - add (29.00) </p>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid lunch">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/takeout/wings.png" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Squad Meal</h4>
+											<h5> 4pcs wingettes & 2pcs drummettes dipped in two flavors of your choice (179.00)</h5>
+											<h5> Box of Fries (129.00)</h5> 
+											<p>Cheese Fondue Overload - add (29.00) </p>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 special-grid lunch">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/takeout/bilao.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Barkada Meal</h4>
+											<h5> 24pcs (799.00)</h5> 
+											<h5> 32pcs (999.00) </h5>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 special-grid lunch">
+									<div class="gallery-single fix">
+										<img src="./public/images/reservation/takeout/add.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4 class="text-center">Additionals</h4>
+											<h5> wings (30.00)</h5> 
+											<h5> rice (30.00) </h5>
+											<h5> heinz ketchup (15.00)</h5> 
+											<h5> Garlic Ranch (20.00) </h5>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+						<div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+							<div class="row">
+								<div class="col-lg-4 col-md-6 special-grid dinner">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-07.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Dinner 1</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $25.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid dinner">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-08.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Dinner 2</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $22.79</h5>
+										</div>
+									</div>
+								</div>
+								
+								<div class="col-lg-4 col-md-6 special-grid dinner">
+									<div class="gallery-single fix">
+										<img src="./public/images/img-09.jpg" data-aos="fade-right"data-aos-offset="300" data-aos-easing="ease-in-sine" class="img-fluid" alt="Image">
+										<div class="why-text">
+											<h4>Special Dinner 3</h4>
+											<p>Sed id magna vitae eros sagittis euismod.</p>
+											<h5> $24.79</h5>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
